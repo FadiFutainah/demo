7e453771d6e3f84d10fbc63e908ed141fa4c4520
Fadi Futainah <fadiftenaft@gmail.com>
2021-02-27 00:54:23 +0200

    first commit

---
+++ src/main/java/com/demo/DemoApplication.java
@@ -0,0 +1,20 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class DemoApplication {
+
+	@RequestMapping(path = "")
+	public String test(){
+		return "<b>test</b>";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
